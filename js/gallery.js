b2e0ca7892275b5f9df258b09470f36f145867bb
Xithariss <joao.vitoor.batista@gmail.com>
2020-11-30 15:29:49 -0300

    Ajustadas as alturas das imagens da galeria e o nome dos personagens já estão sendo mostrados.

--- js/gallery.js
+++ js/gallery.js
@@ -1,40 +1,40 @@
 let accessToken = '868642223878564';
 
 function randomCharacterId() {
-  return Math.round(Math.random() * 731);
+	return Math.round(Math.random() * 731);
 }
 
 const myHeader = new Headers();
 myHeader.append('Content-Type', 'application/json');
 
 const myInit = {
-  method: 'GET',
-  headers: myHeader,
-  mode: 'cors',
+	method: 'GET',
+	headers: myHeader,
+	mode: 'cors',
 };
 
 async function getHeroImage(heroId) {
-  console.log('chamou');
-  let url = `https://superheroapi.com/api.php/${accessToken}/${heroId}/image`;
+	let url = `https://superheroapi.com/api.php/${accessToken}/${heroId}/image`;
 
-  let myRequest = new Request(url, myInit);
+	let myRequest = new Request(url, myInit);
 
-  let response = await fetch(myRequest)      
-  return await response.json();
-  }
+	let response = await fetch(myRequest)
+	return await response.json();
+}
 
-window.onload = async function() {
-  for (let i = 0; i < 8; i++) {
-    console.log('for');
+window.onload = async function () {
+	for (let i = 0; i < 9; i++) {
+		let heroId = randomCharacterId();
 
-    let heroId = randomCharacterId();
+		const imgBoxes = document.querySelectorAll('.box');
 
-    const imgBoxes = document.querySelectorAll('.box');
+		let heroImage = await getHeroImage(heroId);
 
-    let heroImage = await getHeroImage(heroId);
-
-    console.log(heroImage);
-
-    imgBoxes[i].innerHTML = `<a href=${heroImage.url}><img src=${heroImage.url} alt=${heroImage.name} /></a>`;
-  }
+		imgBoxes[i].innerHTML = `
+			<a href=${heroImage.url}>
+				<img src='${heroImage.url}' alt='${heroImage.name}' title='${heroImage.name}' />
+				<span>${heroImage.name}</span>
+			</a>
+		`;
+	}
 }
